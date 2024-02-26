88c016c3b5f723a986d2138aeb9a8de7bad1b8e3
Diego Saavedra <dmsaavedra@espe.edu.ec>
2024-02-25 23:13:39 -0500

    update script.js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function toggleInfo() {
     arrow.innerHTML = "&#x25B6;";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const infoDiv = document.getElementById("info");
+  const toggleButton = document.getElementById("toggleButton");
+
+  toggleButton.addEventListener("click", function () {
+    if (infoDiv.style.display === "none") {
+      infoDiv.style.display = "block";
+    } else {
+      infoDiv.style.display = "none";
+    }
+  });
+});
